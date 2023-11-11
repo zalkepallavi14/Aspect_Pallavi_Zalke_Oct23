2d6aabaff3c4db7ded5591a0881a52bd3aa2e4c3
zalkepallavi14 <148051354+zalkepallavi14@users.noreply.github.com>
2023-11-11 10:24:47 -0800

    Add files via upload

---
+++ ASSIGN10.C
@@ -0,0 +1,46 @@
+/* WAP to perform the operation selected by the
+   user (Using Nested if)*/
+main()
+{
+  int option,num1,num2;
+  clrscr();
+  printf("\n Operation Menu:");
+  printf("\n Addition:");
+  printf("\n Subtraction:");
+  printf("\n Multiplication:");
+  printf("\n Division:");
+  printf("\n Modulus or floor division:");
+  printf("\n Enter your choice:");
+  scanf("%d",&option);
+  if(option<=5)
+  {
+    printf("\n Enter two integer numbers:");
+    scanf("%d%d",&num1,&num2);
+    if(option==1)
+    {
+      printf("\n Addition: %d",num1+num2);
+    }
+    if(option==2)
+    {
+      printf("\n Subtraction: %d",num1-num2);
+    }
+    if(option==3)
+    {
+      printf("\n Multiplication: %d",num1*num2);
+    }
+    if(option==4)
+    {
+      printf("\n Division: %d",num1/num2);
+    }
+    if(option==5)
+    {
+      printf("\n Modulus or floor division: %d",num1%num2);
+    }
+  }
+  else
+  {
+      printf("\n Invalid option selected by you from the operation menu:");
+  }
+  getch();
+}
+
