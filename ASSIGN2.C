33a5c90eef2a59518c1e487e78f3d5e5d947d62b
zalkepallavi14 <148051354+zalkepallavi14@users.noreply.github.com>
2023-11-11 10:09:04 -0800

    Add files via upload

---
+++ ASSIGN2.C
@@ -0,0 +1,37 @@
+/* WAP to find and calculate Addition, subtraction, multiplication,
+   and division of 2 input number.*/
+main()
+{
+   int num1,num2,add,sub,mul,div;
+   clrscr();
+   printf("Enter two number:");
+   scanf("%d%d",&num1,&num2);
+   add=num1+num2;
+   sub=num1-num2;
+   mul=num1*num2;
+   div=num1/num2;
+   clrscr();
+   printf("\n First Number is:%d",num1);
+   printf("\n Second Number is:%d",num2);
+   printf("\n Addition of 2 number is:%d",add);
+
+   printf("\n First Number is:%d",num1);
+   printf("\n Second Number is:%d",num2);
+   printf("\n Subtraction of 2 number is:%d",sub);
+
+   printf("\n First Number is:%d",num1);
+   printf("\n Second Number is:%d",num2);
+   printf("\n Multiplication of 2 number is:%d",mul);
+
+   printf("\n First Number is:%d",num1);
+   printf("\n Second Number is:%d",num2);
+   printf("\n Division of 2 number is:%d",div);
+
+
+
+getch();
+}
+
+
+
+
