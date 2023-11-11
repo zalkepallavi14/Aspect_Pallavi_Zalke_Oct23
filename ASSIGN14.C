3946efda246ec76cc5d272ae99809cb05dcdd276
zalkepallavi14 <148051354+zalkepallavi14@users.noreply.github.com>
2023-11-11 10:30:11 -0800

    Add files via upload

---
+++ ASSIGN14.C
@@ -0,0 +1,13 @@
+/* WAP to display 10 time your name on screen (Using do-while loop).*/
+main()
+{
+   int i=1;
+   clrscr();
+   do
+   {
+      printf("\n Hello");
+      i++;
+   }
+   while(i<=10);
+   getch();
+}
